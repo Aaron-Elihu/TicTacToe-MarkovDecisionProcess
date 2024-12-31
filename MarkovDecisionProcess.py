c8be4a625708efbb1f3dfe5394e109566494bad8
Aaron-Elihu <65421861+Aaron-Elihu@users.noreply.github.com>
2024-12-31 10:00:28 +0100

    Add files via upload

---
+++ MarkovDecisionProcess.py
@@ -0,0 +1,128 @@
+# TicTacToe
+import numpy as np
+import json
+
+
+class MarkovDecisionProcess:
+    def __init__(self):
+        self.states = set()
+        self.T_states = set()
+        self.actions = {}
+        self.policy = open("Policies/policyIteration.json", "r")
+        self.policy = json.load(self.policy)
+
+    # all possible states in game
+    def generate_states(self):
+        # define function to generate all states
+        def _brute_states():
+            # 0: no move, 1: X, 2: O
+            all_board_config = set()
+            for values in np.ndindex(3, 3, 3, 3, 3, 3, 3, 3, 3):
+                state = tuple(values)
+                all_board_config.add(state)
+            return all_board_config
+
+        # define function to check if there are two winners at the same time
+        def _check_2_win(state):
+            count_h, count_v = 0, 0
+            for i in range(3):
+                if (state[i * 3] == state[i * 3 + 1] == state[i * 3 + 2] and state[i * 3] == 1) or (
+                        state[i * 3] == state[i * 3 + 1] == state[i * 3 + 2] and state[i * 3] == 2):
+                    count_h += 1
+                if (state[i] == state[i + 3] == state[i + 6] and state[i] == 1) or (
+                        state[i] == state[i + 3] == state[i + 6] and state[i] == 2):
+                    count_v += 1
+            if count_h == 2 or count_v == 2:
+                return True
+            return False
+
+        # initialise states with possible and impossible states
+        self.states = _brute_states()
+
+        # remove states that are not possible
+        for state in self.states.copy():
+            # suppose that the computer is always X, if no. of X > O, then remove state
+            if state.count(1) > state.count(2):
+                self.states.remove(state)
+            # if abs. difference in the number of X and O > 2, then remove state
+            elif abs(state.count(1) - state.count(2)) > 1:
+                self.states.remove(state)
+            # if there are 2 winners at the same time, then remove state
+            elif _check_2_win(state):
+                self.states.remove(state)
+
+    # terminal states
+    def terminal_state(self):
+        # all possible states and updates states when game is over
+        for state in self.states:
+            if self.win(state):
+                self.T_states.add(state)
+            elif state.count(0) == 0:
+                self.T_states.add(state)
+
+    # action set
+    def generate_actions(self):
+        # define function for all possible states and update the possible actions for each state
+        for state in self.states():
+            self.actions[state] = None
+            if state not in self.T_states:
+                self.actions[state] = []
+                for i in range(9):
+                    if state[i] == 0:
+                        self.actions[state].append(i)
+
+    # transition function
+    def transition_function(self, state):
+        # define function that takes a state and returns the probability of each possible next state
+        # if the game is over
+        if state in self.T_states:
+            return 0
+        # if the game is not over, return 1/(number of possible actions - 1) for 0
+        else:
+            return 1 / (len(self.actions[state]) - 1)
+
+    # reward function
+    def reward_function(self, state):
+        # define function that takes a state and returns the reward of the state
+        if self.win(state) == 1:
+            return 1
+        if self.win(state) == 2:
+            return -1
+        return 0
+
+    def win(self, state):
+        for i in range(3):
+            if state[i * 3] == state[i * 3 + 1] == state[i + 3 + 2] and state[i * 3] != 0:
+                return state[i * 3]
+            if state[i] == state[i + 3] == state[i + 6] and state[i] != 0:
+                return state[i]
+            if state[4] == state[6] == state[8] and state[4] != 0:
+                return state[4]
+            elif state[0] == state[4] == state[8] and state[0] != 0:
+                return state[0]
+            else:
+                return False
+
+    def possible_next_states(self, state, action):
+        new_state = list(state)
+        new_state[action] = 1
+        if self.win(new_state):
+            return []
+        possible_next_states = []
+        for i, case in enumerate(new_state):
+            next_new_state = new_state.copy()
+            if case == 0:
+                next_new_state[i] == 2
+                possible_next_states.append(tuple(next_new_state))
+        return possible_next_states
+
+    def improved_transition_probability(self, state, action):
+        # define function that takes a state and returns the probability of each possible
+        # next state inspired from value iteration policy
+
+        # if the game is over, return 0
+        if state in self.T_states:
+            return 0
+        # if the game is not over, return 1/number of possible actions for 0
+        else:
+            return 1 if action == self.policy[state] else 0
---
+++ TicTacToe.py
@@ -0,0 +1,119 @@
+import tkinter as tk
+import tkinter.messagebox as messagebox
+import json
+
+
+class TicTacToeGUI:
+    def __init__(self, agent="ValueIteration"):
+        self.window = tk.Tk()
+        self.window.title("Tic Tac Toe")
+        self.current_player = "X"  # human player
+        self.computer_symbol = "O"  # computer player
+        self.board = (0,) * 9
+        self.buttons = [None] * 9
+        self.is_vs_computer = False  # flag to check if playing against the computer
+        self.agent = agent
+        self.policy = {}
+        if self.agent == "ValueIteration":
+            path = "Policies/valueIteration.json"
+        elif self.agent == "PolicyIteration":
+            path = "Policies/policyIteration.json"
+        with open(path, 'r') as json_file:
+            self.policy = json.load(json_file)  # open json file as dictionary
+
+        # Calculate the window position to center it on the screen
+        screen_width = self.window.winfo_width()
+        screen_height = self.window.winfo_height()
+        window_width = 320  # adjustable
+        window_height = 430  # adjustable
+        x_axis = (screen_width - window_width) // 2
+        y_axis = (screen_height - window_height) // 2
+        self.window.geometry(f"{window_width}*{window_height}+{x_axis}{y_axis}")
+
+        player_choice = tk.StringVar()
+        player_choice.set("X")
+        player_label = tk.Label(self.window, text="Choose your symbol:")
+        player_label.grid(row=3, columnspan=3)
+        x_radio = tk.Radiobutton(self.window, text="X", variable=player_choice, value="X")
+        o_radio = tk.Radiobutton(self.window, text="O", variable=player_choice, value="O")
+        x_radio.grid(row=4, column=0)
+        o_radio.gird(row=4, column=1)
+
+        # Add a checkbox for playing against the computer
+        vs_computer_checkbox = tk.Checkbutton(self.window, text="Play vs Computer",
+                                              command=self.toogle_vs_computer)
+        vs_computer_checkbox.grid(row=5, columnspan=3)
+
+        start_button = tk.Button(self.window, text="Start Game",
+                                 command=lambda: self.start_game(player_choice.get()))
+        start_button.grid(row=5, columnspan=3)
+
+    def toggle_vs_computer(self):
+        self.is_vs_computer = not self.is_vs_computer
+
+    def start_game(self, player_choice):
+        self.computer_symbol = "O" \
+            if player_choice == "X" else "X"  # set computer symbol opposite to player
+        for i in range(9):
+            row, column = i // 3, i % 3
+            button = tk.Button(self.window, text="", width=10, height=5,
+                               command=lambda idx=i: self.make_move(idx))
+            button.grid(row=row, column=column)
+            self.buttons[i] = button
+
+        if self.computer_symbol == "X" and self.is_vs_computer:
+            # computer starts the game if playing against the computer and computer is X
+            self.make_computer_move()
+
+    def make_move(self, idx):
+        if self.board[idx] == 0:
+            self.board = self.board[:idx] + (1 if self.current_player == "X" else 2,) + self.board[idx + 1:]
+            self.buttons[idx]['text'] = self.current_player
+            if self.check_win() or self.check_tie():
+                self.end_game()
+            else:
+                self.current_player = "O" if self.current_player == "X" else "X"
+                if self.is_vs_computer and self.current_player == self.computer_symbol:
+                    # computer's turn when playing against the computer
+                    self.make_computer_move()
+
+    def make_computer_move(self):
+        state = self.board  # get current state of the board
+        if self.computer_symbol == "O":  # invert X and O if computer is O
+            state = tuple(3-x if x!= 0 else 0 for x in state)
+        self.make_move(self.policy[str(state)])
+
+    def check_win(self):
+        win_combinations = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
+        for combo in win_combinations:
+            a, b, c = combo
+            if self.board[a] == self.board[b] == self.board[c] and self.board[a] != 0:
+                messagebox.showinfo("Game Over", f"Player {self.current_player} wins!")
+                return True
+        return False
+
+    def check_tie(self):
+        if 0 not in self.board:
+            messagebox.showinfo("Game Over", "It's a tie!")
+            return True
+        return False
+
+    def end_game(self):
+        for button in self.buttons:
+            button.config(state=tk.DISABLED)
+
+        restart_button = tk.Button(self.window, text="Restart Game", command=self.restart_game())
+        restart_button.grid(row=7, columnspan=3)
+
+    def restart_game(self):
+        self.window.destroy()
+        self.__init__()
+        self.start_game("X")
+
+    def start(self):
+        self.window.mainloop()
+
+
+if __name__ == "__main__":
+    game = TicTacToeGUI("PolicyIteration")
+    game.start()
